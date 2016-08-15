f787ccfd7b2a8703d7d3f9d1125136acc8dcc01f
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-08-15 18:40:56 -0500

    add users and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  resources :books
+  resources :photos
+  resources :posts
+  resources :users
+
+  get 'home' => 'home#welcome', as: :home
+
+  get 'sign_in' => 'sessions#new', as: :sign_in
+  post 'sign_in' => 'sessions#create'
+  delete 'sign_in' => 'sessions#delete', as: :end_session
+  get 'sign_out' => 'sessions#delete', as: :sign_out
+
+  root 'home#welcome'
+
+
 end
